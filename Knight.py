8fc008fb987b6ba98d1eb4f22c44fff659ba6e8a
seongno Lee <lesn2003@tukorea.ac.kr>
2025-10-25 16:34:17 +0900

    kight portrait 관련 추가 함수 및 변수 knight클래스에 추가

--- Knight.py
+++ Knight.py
@@ -3,16 +3,24 @@ from pico2d import load_image
 
 class Knight:
     image = None
+    p_image = None
     def __init__(self):
         self.x, self.y = 0, 0
+        self.p_x, self.p_y = 0, 0
         self.frame = 0
         self.face_dir = 0
         self.Hp = 1000
         self.Def = 10
         self.Atk = 100
+        self.number = 1
         if self.image is None:
             self.image = load_image('knight_01.png')
+        if self.p_image is None:
+            self.p_image = load_image('Knight_portrait.png')
     def draw(self):
         self.image.clip_draw(0, 0, 100, 100, self.x, self.y)
+    def set_number(self, number):
+        self.number = number
+        self.p_x, self.p_y = (number-1)*100+50, 50
     def update(self):
         pass
