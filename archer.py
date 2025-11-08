bd21a4c95f7487d2b6a621b4f9da7c777a37ce97
seongno Lee <lesn2003@tukorea.ac.kr>
2025-11-09 03:19:18 +0900

    아처 스텟 조정

--- archer.py
+++ archer.py
@@ -4,8 +4,8 @@ from state_machine import StateMachine
 
 
 class Idle:
-    def __init__(self, knight):
-        self.knight = knight
+    def __init__(self, Archer):
+        self.archer = Archer
     def enter(self, e):
         pass
     def exit(self, e):
@@ -13,33 +13,33 @@ class Idle:
     def do(self):
         pass
     def draw(self):
-        x = self.knight.x
-        y = self.knight.y + 50
+        x = self.archer.x
+        y = self.archer.y + 50
         # face_dir == 0 -> 오른쪽, 1 -> 왼쪽(수평 반전)
-        if getattr(self.knight, 'face_dir', 0) == 0:
-            self.knight.image.clip_draw(0, 0, 100, 100, x, y, 150, 160)
+        if getattr(self.archer, 'face_dir', 0) == 0:
+            self.archer.image.clip_draw(0, 0, 100, 100, x, y, 150, 160)
         else:
             # 'h' 플래그로 수평 반전
-            self.knight.image.clip_composite_draw(0, 0, 100, 100, 0, 'h', x, y, 150, 160)
+            self.archer.image.clip_composite_draw(0, 0, 100, 100, 0, 'h', x, y, 150, 160)
 
 
-class Knight:
+class Archer:
     image = None
     def __init__(self):
-        self.depth = 0
+        self.depth = 1
         self.x, self.y = 0, 0
         self.frame = 0
         self.face_dir = 0
-        self.Hp = 1000
+        self.Hp = 700
         self.Def = 10
-        self.Atk = 100
-        self.number = 1
+        self.Atk = 120
+        self.number = 2
         self.tile_w = 100
         self.tile_h = 100
         self.tile_center_x = 0
         self.tile_center_y = 0
         if self.image is None:
-            self.image = load_image('knight_01.png')
+            self.image = load_image('archer_01.png')
         self.IDLE = Idle(self)
 
         self.state_machine = StateMachine(
