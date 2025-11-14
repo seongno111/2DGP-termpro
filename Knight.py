e2794ecaf4cbd590c2b418318c401bbfb9ddb7a5
seongno Lee <lesn2003@tukorea.ac.kr>
2025-11-15 01:22:59 +0900

    기사 Idle상태 애니메이션

--- Knight.py
+++ Knight.py
@@ -1,7 +1,13 @@
 from pico2d import load_image, draw_rectangle
 
+import game_framework
 from state_machine import StateMachine
 
+
+TIME_PER_ACTION = 0.8
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 2
+FRAMES_PER_ACTION_ac = 3
 
 class Idle:
     def __init__(self, knight):
@@ -11,19 +17,22 @@ class Idle:
     def exit(self, e):
         pass
     def do(self):
+        self.knight.frame = (self.knight.frame + FRAMES_PER_ACTION_ac * ACTION_PER_TIME * game_framework.frame_time) % 2
         pass
     def draw(self):
         x = self.knight.x
         y = self.knight.y + 50
         # face_dir == 0 -> 오른쪽, 1 -> 왼쪽(수평 반전)
         if getattr(self.knight, 'face_dir', 0) == 0 or getattr(self.knight, 'face_dir', 0) == 2:
-            self.knight.image.clip_draw(0, 0, 100, 100, x, y, 150, 160)
+            self.knight.image[int(self.knight.frame)].clip_draw(0, 0, 100, 100, x, y, 150, 160)
         else:
             # 'h' 플래그로 수평 반전
-            self.knight.image.clip_composite_draw(0, 0, 100, 100, 0, 'h', x, y, 150, 160)
+            self.knight.image[int(self.knight.frame)].clip_composite_draw(0, 0, 100, 100, 0, 'h', x, y, 150, 160)
 
 class Knight:
-    image = None
+    image = []
+    for i in range(8):
+        image.append(None)
     def __init__(self):
         self.depth = 0
         self.x, self.y = 0, 0
@@ -37,8 +46,14 @@ class Knight:
         self.tile_h = 100
         self.tile_center_x = 0
         self.tile_center_y = 0
-        if self.image is None:
-            self.image = load_image('tuar_01.png')
+        if self.image[0] is None:
+            self.image[0] = load_image('tuar03_01.png')
+            self.image[1] = load_image('tuar03_02.png')
+            self.image[2] = load_image('tuar03_03.png')
+            self.image[3] = load_image('tuar03_04.png')
+            self.image[4] = load_image('tuar03_05.png')
+            self.image[5] = load_image('tuar03_06.png')
+            self.image[7] = load_image('tuar03_07.png')
         self.IDLE = Idle(self)
 
         self.state_machine = StateMachine(
